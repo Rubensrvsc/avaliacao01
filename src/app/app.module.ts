a56a5e25cbe9d74222e03c47301e294b609d4cc7
Rubensrvsc <Rubensspfc100@gmail.com>
2019-06-02 19:16:45 -0300

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ExibirTarefaComponent } from './exibir-tarefa/exibir-tarefa.component';
 import { ExibirListagemComponent } from './exibir-listagem/exibir-listagem.component';
-import { ExibirDetailComponent } from './exibir-detail/exibir-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExibirTarefaComponent,
     ExibirListagemComponent,
-    ExibirDetailComponent,
     DashboardComponent
   ],
   imports: [
